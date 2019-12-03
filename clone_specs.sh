3cc394a9925fa06027aec20a75b37f90b124b7d6
Monica Debbeler <m.b.debbeler@gmail.com>
2019-12-03 16:31:47 -0500

    Point to public version of specs on github

--- clone_specs.sh
+++ clone_specs.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 set -ex
 gem install bundler
-wget https://github.com/wynspeare/http_server_spec/archive/master.tar.gz
+wget https://github.com/mbdebbeler/http_server_spec/archive/master.tar.gz
 tar -xvzf master.tar.gz
 cd http_server_spec-master && bundle install
 bundle exec spinach --tags @simple-get
