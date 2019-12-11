5e6c7aea0ed6cdb491913389d2f51f047036b3a7
Monica Debbeler <m.b.debbeler@gmail.com>
2019-12-11 16:51:10 -0500

    Move scripts to /scripts, test gradle build & unit tests

--- scripts/clone_specs.sh
+++ scripts/clone_specs.sh
@@ -4,4 +4,4 @@ gem install bundler
 wget https://github.com/mbdebbeler/http_server_spec/archive/master.tar.gz
 tar -xvzf master.tar.gz
 cd http_server_spec-master && bundle install
-#bundle exec spinach --tags @simple-get
+bundle exec spinach --tags @simple-get, @simple-head, @not-found
