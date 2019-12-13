9ff55cf5d47be116f63fc861ae2e8a1c5899a81e
Monica Debbeler <m.b.debbeler@gmail.com>
2019-12-13 15:15:03 -0500

    Add sed command to change HOST to EC2 instance's public DNS

--- scripts/clone_specs.sh
+++ scripts/clone_specs.sh
@@ -4,4 +4,5 @@ gem install bundler
 wget https://github.com/mbdebbeler/http_server_spec/archive/master.tar.gz
 tar -xvzf master.tar.gz
 cd http_server_spec-master && bundle install
-bundle exec spinach --tags @simple-get, @simple-head, @not-found
+sed -i -e 's/127.0.0.1/ec2-3-15-197-208.us-east-2.compute.amazonaws.com/' ./features/support/config.yml
+bundle exec spinach --tags @simple-get,@simple-head,@not-found,@simple-options
